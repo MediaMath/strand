07e1e820cbadd45a125c11bd394a4fa6e8bd8258
Anthony Koerber <akoerber@mediamath.com>
2015-06-03 15:31:32 -0500

    Dev/fwrk 291 from ui-framework Squashed commits: [7248536] apply patch to mm-footer from Dev/fwrk 291 from ui-framework

--- src/mm-footer/mm-footer.js
+++ src/mm-footer/mm-footer.js
@@ -7,23 +7,17 @@
 Polymer('mm-footer', {
 	ver:"<<version>>",
 	publish: {
-		message: "",
-		type: "info"
-	},
-
-	messageChanged: function(oldVal, newVal) {
-		// console.log("messageChanged: " + newVal);
-	},
-
-	typeChanged: function(oldVal, newVal) {
-		// console.log("typeChanged: " + newVal);
+		message: null,
+		type: "info",
+		messageVisible: false
 	},
 
 	showMessage: function() {
-		this.$.messageBox.style.display = "block";
+		this.messageVisible = true;
 	},
 
 	hideMessage: function() {
-		this.$.messageBox.style.display = "none";
+		this.messageVisible = false;
 	}
+
 });
