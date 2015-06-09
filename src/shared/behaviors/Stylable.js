6deac0c7af427b5150af6d2306eb809fd1a47032
Dan Lasky <dlasky@mediamath.com>
2015-06-09 12:46:47 -0500

    first pass at styling template methods

---
+++ src/shared/behaviors/Stylable.js
@@ -0,0 +1,24 @@
+(function (scope) {
+	 var Stylable = {
+
+	 	styleList: function(styles) {
+			var out = [];
+			for (var style in styles) {
+				var n = String(style).replace(/[A-Z]/g, function(c) {
+					return '-' + c.toLowerCase();
+				});
+			out.push(n + ': ' + styles[style]);
+			}
+			return out.join('; ');
+	 	},
+
+	 	classList: function(classes) {
+			var out = [];
+			for (var cl in classes) {
+				if (classes[cl]) out.push(key);
+			}
+			return out.join(' ');
+		},
+	 };
+	 scope.Stylable = Stylable;
+})(window.StrandTraits); 
