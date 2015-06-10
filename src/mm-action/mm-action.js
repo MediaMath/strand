fd5ee5b257ef44549c870abc620527dda9d4a358
Anthony Koerber <akoerber@mediamath.com>
2015-06-10 12:39:42 -0500

    Add correct IIF and scope

--- src/mm-action/mm-action.js
+++ src/mm-action/mm-action.js
@@ -4,55 +4,59 @@
  * This code may only be used under the BSD style license found at http://mediamath.github.io/strand/LICENSE.txt
 
 */
-Polymer({
-	is: "mm-action",
+(function (scope) {
 
-	behaviors: [
-		StrandTraits.Stylable
-	],
+	scope.Action = Polymer({
+		is: "mm-action",
 
-	properties: {
-		ver:{
-			type:String,
-			value:"<<version>>",
+		behaviors: [
+			StrandTraits.Stylable
+		],
+
+		properties: {
+			ver:{
+				type:String,
+				value:"<<version>>",
+			},
+			href: {
+				type: String,
+				value: false,
+				reflectToAttribute: true
+			},
+			underline: {
+				type: Boolean,
+				value: false,
+				reflectToAttribute: true
+			},
+			target: {
+				type: String,
+				value: "_self",
+				reflectToAttribute: true
+			},
+			disabled: {
+				type: Boolean,
+				value: false,
+				reflectToAttribute: true
+			}
 		},
-		href: {
-			type: String,
-			value: false,
-			reflectToAttribute: true
+
+		PRIMARY_ICON_COLOR: Colors.D0,
+
+		ready: function() {
+			// if there is an icon - colorize it:
+			var items = Array.prototype.slice.call(Polymer.dom(this.$.icon).getDistributedNodes());
+			if (items.length) {
+				items[0].setAttribute("primary-color", this.PRIMARY_ICON_COLOR);
+			}
 		},
-		underline: {
-			type: Boolean,
-			value: false,
-			reflectToAttribute: true
-		},
-		target: {
-			type: String,
-			value: "_self",
-			reflectToAttribute: true
-		},
-		disabled: {
-			type: Boolean,
-			value: false,
-			reflectToAttribute: true
+
+		updateClass: function(underline) {
+			var o = {};
+			o["action"] = true;
+			o["underline"] = underline;
+			return this.classList(o);
 		}
-	},
 
-	PRIMARY_ICON_COLOR: Colors.D0,
-
-	ready: function() {
-		// if there is an icon - colorize it:
-		var items = Array.prototype.slice.call(Polymer.dom(this.$.icon).getDistributedNodes());
-		if (items.length) {
-			items[0].setAttribute("primary-color", this.PRIMARY_ICON_COLOR);
-		}
-	},
-
-	updateClass: function(underline) {
-		var o = {};
-		o["action"] = true;
-		o["underline"] = underline;
-		return this.classList(o);
-	}
-
-});
+	});
+	
+})(window.Strand = window.Strand || {});
