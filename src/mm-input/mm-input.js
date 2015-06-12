e0bec42537c0a3fa159d219c65f2926ef637c028
Anthony Koerber <akoerber@mediamath.com>
2015-06-12 17:02:13 -0500

    WIP mm-input port
    mm-input port to 1.0 complete
    
    cleanup mm-input 1.0 port
    
    Add validate behavior
    
    Code review
    - Strip unneeded items
    - Add validator as behavior
    - Change property "state" to "direction" for future integration with Closable
    
    Updates to validation
    - Rename
    - Handle all validation in Validatable
    
    Updates & Cleanup
    - Update doc.json
    - Get maxlength working
    
    addt'l cleanup
    
    mm-textara port
    
    Declarative approach to showing and hiding local DOM
    
    underscore privates
    
    updates to Validatable
    
    More CR Feedback
    - Remove unneeded items in Validatable.js
    - Make sure "empty" validation gives visual feedback
    - Move non-bound constants out of properties
    - Underscore privates in mm-textarea
    
    Modified to input and Validatable
    - Change name to Validatable
    - Additional check for validation if validate is called directly
    - Add mixing for invalid/error state

--- src/mm-input/mm-input.js
+++ src/mm-input/mm-input.js
@@ -4,213 +4,155 @@
  * This code may only be used under the BSD style license found at http://mediamath.github.io/strand/LICENSE.txt
 
 */
-Polymer('mm-input', {
-	ver:"<<version>>",
-	//defaults
-	STATE_OPENED: "opened",
-	STATE_CLOSED: "closed",
-	PRIMARY_ICON_COLOR: Colors.A18,
-	PADDING_ICON: 25,
+(function (scope) {
 
-	searchItems:[],
-	state: "closed",
-	placeholder:"",
-	validation:"none",
-	overflow: "hidden", // default width takes precedence
-	type: "text", // input default type
-	icon: "search", // default icon type 
-	search: false,
-	clear: false,
-	error: false,
-	autoItemSelected: false,
-	autocomplete: false,
-	paddingRight: 10,
+	scope.Input = Polymer({
+		is: 'mm-input',
 
-	publish: {
-		value: { value: "", reflect: true },
-		disabled: { value: false, reflect: true },
-		fitparent: { value: false, reflect: true },
-		layout: { value: null, reflect: true },
-		data: []
-	},
+		behaviors: [
+			StrandTraits.Stylable,
+			StrandTraits.Validatable
+		],
 
-	ready: function() {
-		this.email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		this.alpha = /^\w+$/;
-		this.inter = /^\d+$/;
-		this.floater = /^\d*[.]\d+$/;
-	},
-
-	dataChanged: function(oldData, newData) {
-		if (typeof newData === "string") {
-			try {
-				this.data = JSON.parse(newData);
-			} catch(e) {
-				this.data = newData;
+		properties: {
+			PRIMARY_ICON_COLOR: {
+				type:String, 
+				value: Colors.A18 
+			},
+			direction: {
+				type: String,
+				value: false
+			},
+			placeholder: {
+				type: String,
+				reflectToAttribute: true
+			},
+			type: {
+				type: String,
+				value: "text",
+				reflectToAttribute: true,
+				observer: "_typeChanged"
+			},
+			icon: {
+				type: String,
+				value: false,
+				reflectToAttribute: true
+			},
+			search: {
+				type: Boolean,
+				value: false,
+				reflectToAttribute: true,
+				observer: "_searchChanged"
+			},
+			clear: {
+				type: Boolean,
+				value: false,
+				reflectToAttribute: true
+			},
+			width: {
+				type: Number,
+				value: false,
+				reflectToAttribute: true
+			},
+			maxlength: { 
+				type: Number,
+				value: false, 
+				reflectToAttribute: true 
+			},
+			value: {
+				type: String,
+				value: null,
+				observer: "_valueChanged",
+				notify: true
+			},
+			disabled: { 
+				type: Boolean,
+				value: false, 
+				reflectToAttribute: true 
+			},
+			fitparent: { 
+				type: Boolean,
+				value: false, 
+				reflectToAttribute: true
+			},
+			layout: { 
+				type: String,
+				value: false, 
+				reflectToAttribute: true 
+			},
+			clearVisible: {
+				type: Boolean,
+				value: false
 			}
-		}
-	},
+		},
 
-	typeChanged: function(oldType, newType) {
-		var types = /(text|password|email|number|tel|search|url)/ig;
-		if (!types.test(newType)) {
-			this.type = "text";
-		}
-	},
+		DIRECTION_TOP:  "top",
+		DIRECTION_BOTTOM: "bottom",
+		PADDING_RIGHT_ICON: 25,
+		PADDING_RIGHT_DEFAULT: 10,
 
-	searchChanged: function(oldVal, newVal) {
-		if(this.icon === "search" && newVal) {
-			this.paddingRight = this.PADDING_ICON;
-			this.$.icon.style.display = "block";
-		}
-	},
+		_typeChanged: function(newVal, oldVal) {
+			var types = /(text|password|email|number|tel|search|url)/ig;
+			if (!types.test(newVal)) {
+				this.type = "text";
+			}
+		},
 
-	iconChanged: function(oldVal, newVal) {
-		if(this.icon !== "search" && newVal) {
-			this.paddingRight = this.PADDING_ICON;
-			this.$.icon.style.display = "block";
-		}
-	},
+		_searchChanged: function(newVal, oldVal) {
+			if (newVal) { 
+				this.icon = "search";
+			} else if (!newVal && this.icon === "search") {
+				this.icon = false;
+			}
+		},
 
-	clearChanged: function(oldVal, newVal) {
-		this.paddingRight = this.PADDING_ICON;
-	},
-	
-	valueChanged: function(oldVal, newVal) {
-		//update model if needed
-		this.updateModel();
-		this.fire("changed", { value: this.value });
+		_updateIcon: function(icon, clearVisible) {
+			var visible = icon && !clearVisible;
+			return this.styleBlock({
+				display: visible ? "block" : "none"
+			});
+		},
+
+		_updateClear: function(clearVisible) {
+			return this.styleBlock({
+				display: clearVisible ? "block" : "none"
+			});
+		}, 
 		
-		if (!this.autoItemSelected){
-			// the user is typing
-			this.validate();
-			this.updateUI();
-		} else {
-			// the user selected an item from the autocomplete list
-			this.autoItemSelected = false;
-		}
-	},
-
-	inputWidthChanged: function (oldVal, newVal) {
-		// set close panel width if the input width changed:
-		if (this.$ && this.$.closePanel) {
-			if (this.autocomplete && this.overflow === "visible") {
-				this.$.closePanel.style.width = "auto";
-				this.$.closePanel.style.minWidth = newVal + "px";
+		_valueChanged: function(newVal, oldVal) {
+			if (newVal && newVal.length > 0) {
+				this.clearVisible = this.clear;
 			} else {
-				this.$.closePanel.style.width = newVal + "px";
+				this.clearVisible = false;
 			}
+			this.fire("changed", { value: newVal });
+		},
+
+		clearInput: function(e) {
+			this.value = null;
+			this.clearVisible = false;
+		},
+
+		_updateStyle: function(icon, width, fitparent, clear) {
+			var p = icon || clear ? this.PADDING_RIGHT_ICON + "px" : this.PADDING_RIGHT_DEFAULT + "px",
+				f = fitparent ? "100%" : false,
+				w = width ? width + "px" : "auto";
+
+			return this.styleBlock({
+				paddingRight: p,
+				width: f ? f : w
+			});
+		},
+
+		_updateClass: function(direction, error) {
+			var o = {};
+			o["text-input"] = true;
+			o.invalid = error;
+			o.top = (direction === this.DIRECTION_TOP);
+			o.bottom = (direction === this.DIRECTION_BOTTOM);
+			return this.classBlock(o);
 		}
-	},
 
-	validate: function() {
-		var valid = this.validator();
+	});
 
-		if (valid) {
-			this.error = false;
-		} else {
-			this.error = true;
-		}
-	},
-
-	validator: function() {
-		switch(this.validation) {
-			case "empty":
-				return this.value.length > 0;
-			case "int":
-				return this.inter.test(this.value);
-			case "decimal":
-				return this.floater.test(this.value);
-			case "email":
-				return this.email.test(this.value);
-			case "alpha":
-				return this.alpha.test(this.value);
-			default:
-				return true;
-		}
-	},
-
-	updateUI: function() {
-		var dataFilter = function(a) {
-			return a.name.indexOf(this.value) != -1;
-		};
-
-		//handle the clear and search state styling:
-		if (this.value && this.value.length > 0) {
-			if (this.clear) {
-				this.$.clearBtn.style.display = "block";
-			}
-			if (this.icon === "search" && this.search && this.clear || this.icon && this.clear) {
-				this.$.icon.style.display = "none";
-			}
-			if (this.autocomplete) {
-				this.searchItems = this.data.filter(dataFilter.bind(this));
-				if (this.searchItems.length > 0) {
-					this.open();
-				}
-				// if (this.searchItems.length > this.maxItems) {
-				// this.searchItems = this.searchItems.splice(0, this.maxItems);
-				// }
-			}
-		} else {
-			if (this.clear) {
-				this.$.clearBtn.style.display = "none";
-			}
-			if (this.icon === "search" && this.search && this.clear || this.icon && this.clear) {
-				this.$.icon.style.display = "block";
-			}
-			if (this.autocomplete) {
-				this.close();
-			}
-		}
-	},
-	
-	bindModel: function(model, property) {
-		this.model = model;
-		this.property = property;
-	},
-
-	updateModel: function() {
-		if (this.model && this.property) {
-			//check for BB models
-			if (this.model.set) {
-				var o = {};
-				o[this.property] = this.value;
-				this.model.set(o);
-			} else {
-				this.model[this.property] = this.value;
-			}
-		}
-	},
-
-	clearInput: function(e) {
-		this.value = "";
-	},
-
-	get inputWidth() {
-		if (this.$)
-		return parseFloat(Measure.getComputedStyle(this.$.input).width);
-	},
-
-	open: function(e) {
-		this.state = this.STATE_OPENED;
-	},
-
-	close: function(e) {
-		this.state = this.STATE_CLOSED;
-	},
-
-	onSelectItem: function(e) {
-		this.autoItemSelected = true;
-		this.value = e.detail.item.textContent.trim();
-		this.close();
-	},
-
-	getWidth: function(value) {
-		if(value && !this.fitparent) {
-			return value + "px";
-		} else {
-			return "100%";
-		}
-	}
-});
+})(window.Strand = window.Strand || {});
