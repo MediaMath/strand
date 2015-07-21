6587754553a0cf8434d173c520b114277c494cd4
Dan Lasky <dlasky@mediamath.com>
2015-07-21 10:44:05 -0500

    ajax wip

--- src/mm-ajax/mm-ajax.js
+++ src/mm-ajax/mm-ajax.js
@@ -7,173 +7,84 @@
 /*jshint -W030 */
 (function (scope) {
 
-	function _paramaterize(name, value) {
-		return encodeURIComponent(name) + "=" + encodeURIComponent(value);
-	}
-
-	function _serialize(data, parent, depth, paramaterize) {
-
-		paramaterize = paramaterize || _paramaterize;
-		var obj, 
-			output = [];
-		parent = parent || "";
-		depth = depth || 0;
-
-		for(var i in data) {
-			obj = data[i];
-			name = (depth === 0) ? i : "[" + i + "]";
-			if (typeof obj === "object") {
-				output = output.concat( _serialize(obj, name, depth+1) );
-			} else {
-				output.push( paramaterize(parent + name, obj) );
-			}
-		}
-		return output;
-	}
-
 	scope.Ajax = Polymer({
 		is: 'mm-ajax',
 
-		ver:"<<version>>",
-
-		GET: "GET",
-		POST: "POST",
-		PUT: "PUT",
-		DELETE: "DELETE",
+		GET: StrandLib.Ajax.GET,
+		POST: StrandLib.Ajax.POST,
+		PUT: StrandLib.Ajax.PUT,
+		DELETE: StrandLib.Ajax.DELETE,
 
 		properties: {
+			ajax: {
+				type:StrandLib.Ajax,
+				value: function() {
+					return new StrandLib.Ajax();
+				}
+			},
 			contentType:{
 				type:String,
-				value: 'application/x-www-form-urlencoded'
+				value: 'application/x-www-form-urlencoded',
+				observer:"_updateAjax"
 			},
 			method: {
 				type:String,
-				value: "GET"
+				value: "GET",
+				observer:"_updateAjax"
 			},
 			auto: {
 				type:Boolean,
 				value:false,
+				observer:"_updateAjax"
 			},
 			body: {
 				type:Object,
-				value: null
+				value: null,
+				observer:"_updateAjax"
 			},
 			withCredentials: {
 				type: Boolean,
-				value: false
+				value: false,
+				observer:"_updateAjax"
 			},
 			timeout: {
 				type:Number,
-				value: 10000
+				value: 10000,
+				observer:"_updateAjax"
 			}
-		},
-
-		// publish: {
-		// 	contentType: 'application/x-www-form-urlencoded',
-		// 	method: "GET",
-		// 	auto: false,
-		// 	debug: false,
-		// 	body: null,
-		// 	withCredentials: false,
-		// 	timeout: 10000
-		// },
-
-		created: function() {
-			this.reset();
 		},
 
 		ready: function() {
 			if (this.auto && this.url && this.method) {
-				this.exec();
+				this.ajax.exec();
 			}
 		},
 
-		reset: function() {
-			this._xhr = new XMLHttpRequest();
-			this.promise = pinkySwear();
-			this.headers = [];
-			this.params = [];
-			this.urlParams = [];
+		_updateAjax: function() {
+			//TODO(dlasky): expose binds for additional prefs
+			this.ajax.options = DataUtils.copy(this.ajax.options, {
+				contentType:this.contentType,
+				method:this.method,
+				body:this.body,
+				withCredentials:this.withCredentials,
+				timeout:this.timeout
+			});
 		},
 
-		exec: function() {
-			var data,
-				fd,
-				message,
-				url = this.url;
-
-			if (!this.method || !url) {
-				message = "url and method are required!!";
-				this.fire("data-error", {message:message});
-				this.promise(false, [this, this.promise, message]);
-				return this.promise;
+		_validateAjax: function() {
+			if (!this.method) return false;
+			if (this.method === StrandLib.Ajax.GET) {
+				if (this.url) return true;
+			} else {
+				if (this.url && this.body) return true;
 			}
-
-			if (this.urlParams && this.urlParams.length > 0) {
-				if (url.slice(-1) === "/") {
-					url += this.serializeUrl(this.urlParams);
-				} else {
-					url += "/" + this.serializeUrl(this.urlParams);
-				}
-			}
-
-			if (this.method.match(/(POST|PUT|DELETE)/i)) {
-				if (this.contentType === "application/json") {
-					data = this.body;
-				} else if(this.contentType === "multipart/form-data") {
-					if (this.body instanceof FormData) {
-						data = this.body;
-					} else {
-						//TODO: expose this outside of body
-						fd = new FormData();
-						if (this.body instanceof FormData) {
-							data = this.body;
-						} else {
-							this.serialize(this.body, fd.append);
-							data = fd;
-						}
-					}
-				} else {
-					data = this.serialize( this.body ).join("&");
-				}
-			} 
-
-			if (this.params && this.params.length) {
-				if (url.indexOf("?") !== -1) {
-					var last = url.slice(-1);
-					if (last !== "&") {
-						url += "&";
-					}
-				} else {
-					url += "?";
-				}
-				url += this.serialize(this.params).join("&");
-			}
-
-			//open(DOMString method, DOMString url, optional boolean async, optional DOMString? user, optional DOMString? password);
-			this.xhr.open(this.method, url, true, this.username, this.password);
-
-			this.xhr.timeout = this.timeout || 10000;
-			this.xhr.withCredentials = this.withCredentials;
-			this.xhr.onreadystatechange = this.readyStateChange.bind(this);
-			this.xhr.addEventListener("abort", this.handleAbort.bind(this));
-			this.xhr.addEventListener("progress", this.handleProgress.bind(this));
-
-			this.contentType && this.addHeader("content-type",this.contentType);
-
-			this.headers && this.headers.forEach(function(header) { 
-				this.xhr.setRequestHeader(header.name, header.value); 
-			}.bind(this));
-
-			this.xhr.send(data);
-
-			return this.promise;
-
+			return false;
 		},
-		
+
 		abort: function() {
-			if (this.xhr) {
-				this.xhr.abort();
+			this.ajax.abort();
+			if (this.ajax.current) {
+				this.ajax.current.promise.then(handleAbort, handleAbort);
 			}
 		},
 
@@ -193,116 +104,28 @@
 		},
 
 		addHeader: function(name, value) {
-			this.headers = this.headers || [];
-			this.headers.push({name:name, value:value});
+			this.ajax.addHeader(name, value);
 		},
 
 		addParam: function(name, value) {
-			this.params = this.params || [];
-			this.params.push({name:name, value:value});
+			this.ajax.addParam(name,value);
 		},
 
 		addUrlParam: function(param) {
-			this.urlParams = this.urlParams || [];
-			this.urlParams.push(param);
-		},
-
-		serializeUrl: function(array) {
-			return array.join("/");
-		},
-
-		serialize: function(data, paramaterize) {
-
-			paramaterize = paramaterize || _paramaterize;
-			var i, obj, output = [];
-			if (typeof data === "string") {
-				try {
-					data = JSON.parse(data);
-				} catch(e) {
-					this.fire("data-error", {error: e.message, instance: this});
-				}
-			}
-			if ( DataUtils.isType(data, "array") ) {
-				for (i = data.length - 1; i >= 0; i--) {
-					obj = data[i];
-					if (obj.name !== null && obj.value !== null) {
-						output.push( paramaterize(obj.name, obj.value) );
-					}
-				}
-
-			} else {
-				output = _serialize(data, null);
-			}
-			
-			return output;
-		},
-
-		readyStateChange: function() {
-			var result = {};
-			if (this.xhr.readyState === 4) {
-				this.fire("data-result");
-				this.response = this.responseHandler[this.responseType || 'json'].call(this);
-				if (!this.xhr.status || (this.xhr.status >= 200 && this.xhr.status < 300)) {
-					result = {response: this.response, instance: this};
-					this.fire("data-ready", result);
-					if (this.promise) {
-						this.promise(true, [this, this.promise, result]);
-					}
-				} else {
-					result = {error: this.xhr.status, instance: this, response: this.response};
-					this.fire("data-error", result);
-					if (this.promise) {
-						this.promise(false, [this, this.promise, result]);
-					}
-				}
-			}
-		},
-
-		responseHandler: {
-
-			xml: function() {
-				return this.xhr.responseXML;
-			},
-
-			text: function() {
-			return this.xhr.responseText;
-			},
-
-			json: function() {
-				var response = this.xhr.responseText;
-				try {
-					return JSON.parse(response);
-				} catch (e) {
-					this.fire("data-error", {error: e.message, instance: this});
-					return response;
-				}
-			},
-
-			document: function() {
-				return this.xhr.response;
-			},
-
-			blob: function() {
-				return this.xhr.response;
-			},
-
-			arrayBuffer: function() {
-				return this.xhr.response;
-			},
-
+			this.ajax.addUrlParam(param);
 		},
 
 		get status() {
-			return this.xhr && this.xhr.status;
+			return this.ajax.status;
 		},
 
 		get state() {
-			return this.xhr && this.xhr.readyState;
+			return this.ajax.state;
 		},
 
 		get xhr() {
-			return this._xhr;
+			return this.ajax.xhr;
 		}
 	});
 
-})(window.Strand = window.Strand || {}); 
+})(window.Strand = window.Strand || {});
