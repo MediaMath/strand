6a9366ea0cee0b1f79dfe13d0e161dd7a402e8e3
Shuwen Qian <sqian@mediamath.com>
2015-10-16 17:57:07 -0400

    Tweaks/notes on drag and drop reordering functionality

--- src/mm-tag-list/mm-tag-list.js
+++ src/mm-tag-list/mm-tag-list.js
@@ -6,6 +6,7 @@
 
 		properties: {
 			data: {
+				// Should add a _sortIndex to all the items here so we can keep track of order independent of DOM/how they are ordered in the array
 				type: Array,
 				value: function() { return []; },
 				notify: true
@@ -42,17 +43,24 @@
 		_handleDrag: function(e) {
 			if(e.target.classList.contains('tag')) this._tagToMove = e.target;
 
-			var tagRect = this._tagToMove.getBoundingClientRect();
+			this._tagToMove.style.opacity = '0';
+			this._tagToMove.style.position = 'absolute';
+
 
 			if(!this._placeholder) {
+				// TODO: Do this without writing css in js
 				var ph = document.createElement('li');
+				var tagRect = this._tagToMove.getBoundingClientRect();
+				ph.textContent = '.';
 				ph.className = 'placeholder';
-				ph.textContent = 'test';
-
+				ph.style.boxSizing = 'border-box';
 				ph.style.borderRadius = '2px';
+				ph.backgroundColor = '#999999';
 				ph.style.display = 'inline-block';
+				ph.style.margin = this._tagToMove.style.margin;
 				ph.style.height = tagRect.height+"px";
 				ph.style.width = tagRect.width+"px";
+				ph.style.position = 'relative';
 
 				this.$.list.appendChild(ph);
 				this._placeholder = ph;
@@ -60,15 +68,22 @@
 		},
 
 		_handleDragenter: function(e) {
-			if(e.target !== this._tagToMove && e.target !== this._tagToMove.nextElementSibling && e.target.classList.contains('tag')) {
+			if(e.target !== this._tagToMove && e.target.classList.contains('tag')) {
 				this._dropTarget = e.target;
 				if(this._placeholder) this.$.list.insertBefore(this._placeholder, this._dropTarget);
 			}
 		},
 
 		_handleDragend: function() {
+			// TODO: Reflect these changes to the data model
 			this.$.list.insertBefore(this._tagToMove, this._placeholder);
 			this.$.list.removeChild(this._placeholder);
+			this._placeholder = null;
+
+			this._tagToMove.style.opacity = null;
+			this._tagToMove.style.position = null;
+
+			console.table(this.data);
 		},
 
 	});
