1930f9223d03b4f6e3bca27d00ac5104a6961f33
Dan Lasky <dlasky@mediamath.com>
2015-11-10 17:08:38 -0600

    changed files only

--- Gulpfile.js
+++ Gulpfile.js
@@ -16,26 +16,35 @@ var run = require('run-sequence');
 var htmlmin = require('gulp-minify-html');
 var wrap = require('gulp-wrap');
 var inlineAssets = require('gulp-inline-assets');
+var marked = require('gulp-marked');
+var changed = require('gulp-changed');
+var es = require('event-stream');
+var cache = require('gulp-cached');
 
 var SRC = 'src/';
 var BUILD = 'build/';
-var DOCS = 'build_docs/';
+var BUILD_DOCS = 'build_docs/';
 var DIST = 'dist/';
 var TEMPLATES = 'grunt/templates/'; //TODO swap to gulp
 
 /** BUILD **/
 
 gulp.task('clean', function() {
-	return del(BUILD, DOCS, DIST);
+	return del([BUILD + '**', BUILD_DOCS+ '**', DIST+ '**']);
 });
 
 gulp.task('copy', function() {
 	return gulp.src([SRC + '**/*.+(html|js|woff)', '!' + SRC +'**/example.html'])
+		.pipe(changed(BUILD))
+		.pipe(debug())
 		.pipe(gulp.dest(BUILD));
 });
 
 gulp.task('sass', function() {
 	return gulp.src(SRC + 'mm-*/*.scss')
+		// .pipe(cache())
+		.pipe(changed(BUILD, {extension:'.css'}))
+		.pipe(debug())
 		.pipe(sass({includePaths: ['./bower_components/bourbon/app/assets/stylesheets/', './src/shared/sass/']}).on('error', sass.logError))
 		.pipe(gulp.dest(BUILD))
 		.pipe(wrap({src:TEMPLATES + "style_module_template.html"},{},{engine:"hogan"}))
@@ -54,6 +63,7 @@ gulp.task('font', function() {
 
 gulp.task('vulcanize', function() {
 	var modules = gulp.src(BUILD + "mm-*/mm-*.html")
+		.pipe(changed(BUILD))
 		.pipe(vulcanize({
 			inlineScripts:true,
 			inlineCss:true,
@@ -85,6 +95,10 @@ gulp.task('default', function(cb) {
 	run('clean','copy',['sass','font'],'vulcanize',cb);
 });
 
+gulp.task('build', function(cb) {
+	run('copy', ['sass','font'],'vulcanize', cb);
+});
+
 gulp.task('vulcanize:prod', function() {
 	return gulp.src(BUILD + "mm-*/mm-*.html")
 		.pipe(vulcanize({
@@ -104,12 +118,23 @@ gulp.task('vulcanize:prod', function() {
 
 gulp.task('docs', function() {
 	var modules = glob.sync("mm-*", {cwd:SRC});
+	var articles = glob.sync("*.md", {cwd:"./docs/articles/"});
+	var articleMap = JSON.parse(fs.readFileSync('./docs/articles/manifest.json'));
+
+	var articleStream = gulp.src('./docs/articles/*.md')
+		.pipe(debug())
+		.pipe(marked().on('error',console.log))
+		.pipe(wrap({src:"./docs/article_template.html"},{},{engine:"hogan"}).on('error',console.log))
+		.pipe(gulp.dest(BUILD_DOCS));
+	return articles;
 });
 
 /** LIVE **/
 
-gulp.task('sass:watch', function () {
- gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+ gulp.watch(SRC + 'mm-*/*.scss', ['sass']);
+ // gulp.watch(SRC)
+ // gulp.watch('./')
 });
 
 /** DEPLOY **/
