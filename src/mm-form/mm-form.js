f29e4daec5d4c0cca42f62e3650e29408e9ae13c
Anthony Koerber <akoerber@mediamath.com>
2015-12-07 17:22:53 -0600

    first pass at config and functionality

--- src/mm-form/mm-form.js
+++ src/mm-form/mm-form.js
@@ -25,6 +25,13 @@
 				value: 10,
 				reflectToAttribute: true
 			},
+			// submitMessage: {
+			// 	type: String,
+			// 	notify: true
+			// },
+			formItems: {
+				type: Array
+			},
 			formData: {
 				type: Object,
 				value: function() { return {}; }
@@ -37,7 +44,67 @@
 
 		ready: function() {
 			// build form data object
-			
+			// TODO: consider effective children apis once we get to v1.2.3
+			// https://www.polymer-project.org/1.0/docs/devguide/local-dom.html#effective-children
+			this.async(function() {
+				this.formItems = Polymer.dom(this).querySelectorAll('[form-id]');
+				this.formItems.forEach(function(item) {
+					this.formData[item.formId] = item.value;
+				}.bind(this));
+				console.log("this.formData: ", this.formData);
+			});
+		},
+
+		submitForm: function() {
+			var invalid = [],
+				valid = [];
+
+			// UI validation pass:
+			// console.log('submitForm');
+			this.formItems.forEach(function(item){
+				// update the form data object:
+				this.formData[item.formId] = item.value;
+
+				// validate UI:
+				var isValid = item.validate(item.value);
+
+				// track invalids
+				if (!isValid) {
+					invalid.push(item.formId);
+				} else {
+					valid.push(item.formId);
+				}
+
+				// show error state:
+				item.error = !isValid;
+
+				// show target message:
+				if(item._errorTarget && !isValid) {
+					// item._errorTarget.display();
+					item._errorTarget.style.display = 'block';
+				} else {
+					// item._errorTarget.hide();
+					item._errorTarget.style.display = 'none';
+				}
+			}.bind(this));
+
+			if (invalid.length > 0) {
+				// there were errors
+				// show footer error
+				// console.log('invalids:', invalids);	
+				this.submitMessage = 'This form contains errors';
+			} else {
+				// send the data to some endpoint
+				// handle that response
+			}
+
+			// fire an invalid form event:
+			this.fire('form-submit', {
+				isValid: !invalid.length > 0,
+				invalidFields: invalid,
+				validFields: valid,
+				data: this.formData 
+			});
 		},
 
 		// styling concerns (columns)
@@ -60,7 +127,7 @@
 					item.style.marginBottom = spacing + 'px';
 				});
 			}
-		}
+		},
 	
 	});
 
