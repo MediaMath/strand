6783ece59faf661146742d00dc3ada4f9673e6ab
Anthony Koerber <akoerber@mediamath.com>
2016-01-11 18:28:53 -0600

    NEW mm-form WIP

--- src/mm-form/mm-form.js
+++ src/mm-form/mm-form.js
@@ -34,18 +34,32 @@
 		*/	
 
 		properties: {
-			formItems: {
+			
+			unsaved: {
+				type: Boolean,
+				value: false,
+				notify: true
+			},
+
+			showUnsavedMessage: {
+				type: Boolean,
+				value: true,
+				notify: true
+			},
+
+			// what comes in:
+			data: {
+				type: Object,
+				observer: '_dataChanged'
+			},
+
+			// will store updates/serialize - data becomes:
+			_formData: {
 				type: Object,
 				value: function() { return {}; }
 			},
-			formData: {
-				type: Object,
-				value: function() { return {}; },
-			},
-			footerMessage: {
-				type: String,
-				notify: true
-			},
+
+			// Footer related
 			footerMessages: {
 				type: Object,
 				value: function() {
@@ -56,10 +70,7 @@
 					};
 				}
 			},
-			footerType: {
-				type: String,
-				notify: true
-			},
+
 			actions: {
 				type: Array,
 				value: function() {
@@ -82,140 +93,143 @@
 					];
 				}
 			},
-			unsaved: {
-				type: Boolean,
-				value: false,
-				notify: true
-			},
+
 			showFooterMessages: {
 				type: Boolean,
 				value: true,
 				notify: true
 			},
-			showWarningMessages: {
-				type: Boolean,
-				value: true,
+
+			_footerType: {
+				type: String,
 				notify: true
 			},
+
+			_footerMessage: {
+				type: String,
+				notify: true
+			},
+
 			_showFooterMessage: {
 				type: Boolean,
 				value: false,
 				notify: true
 			},
+
 			_displayFooterMessage: {
 				type: Boolean,
 				computed: '_displayMessage(_showFooterMessage, showFooterMessages)'
 			}
+
 		},
 
 		listeners: {
 			'changed' : '_handleChanged'
 		},
 
-		// common validation rules
-		rules: {
-			email: function(i) {
-				// var regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-				// return regEx.test(i);
-				return validator.isEmail(i);
-			},
-			alpha: function(i) {
-				// var regEx = /^\w+$/;
-				// return regEx.test(i);
-				return validation.isAlpha(i);
-			},
-			int: function(i) {
-				// var regEx = /^\d+$/;
-				// return regEx.test(i);
-				return validator.isInt(i); 
-			},
-			decimal: function(i) {
-				// var regEx = /^\d*[.]\d+$/;
-				// return regEx.test(i);
-				return validator.isDecimal(i);
-			},
-			whitespace: function(i) {
-				// var regEx = /\s/;
-				// return i.length > 0 && !regEx.test(i);
-				return validator.isWhitespace(i);
-			},
-			checked: function(i) {
-				return validator.isChecked(i);
-			},
-			empty: function(i) {
-				return validator.isEmpty(i);
-			},
-			blank: function(i) {
-				return validator.isBlank(i);
-			}
-		},
-
-		_initialFormData: {},
+		// // common validation rules
+		// rules: {
+		// 	email: function(i) {
+		// 		// var regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		// 		// return regEx.test(i);
+		// 		return validator.isEmail(i);
+		// 	},
+		// 	alpha: function(i) {
+		// 		// var regEx = /^\w+$/;
+		// 		// return regEx.test(i);
+		// 		return validation.isAlpha(i);
+		// 	},
+		// 	int: function(i) {
+		// 		// var regEx = /^\d+$/;
+		// 		// return regEx.test(i);
+		// 		return validator.isInt(i); 
+		// 	},
+		// 	decimal: function(i) {
+		// 		// var regEx = /^\d*[.]\d+$/;
+		// 		// return regEx.test(i);
+		// 		return validator.isDecimal(i);
+		// 	},
+		// 	whitespace: function(i) {
+		// 		// var regEx = /\s/;
+		// 		// return i.length > 0 && !regEx.test(i);
+		// 		return validator.isWhitespace(i);
+		// 	},
+		// 	checked: function(i) {
+		// 		return validator.isChecked(i);
+		// 	},
+		// 	empty: function(i) {
+		// 		return validator.isEmpty(i);
+		// 	},
+		// 	blank: function(i) {
+		// 		return validator.isBlank(i);
+		// 	}
+		// },
 
 		// *******************************
 		// collect all the things (and data)
+		// _canInit: false,
+
 		attached: function() {
-			// build form data object
-			this.async(function() {
-				var formFields = Polymer.dom(this).querySelectorAll('[name]');
-
-				formFields.forEach(function(item) {
-					var name 			= item.getAttribute('name'),
-						field 			= item,
-						label 			= item.getAttribute('label'),
-						value 			= item.value,
-						validation 		= item.getAttribute('validation'),
-						errorMsg 		= item.getAttribute('error-message'),
-						errorMsgEle		= null,
-						fieldHeaderEle 	= null,
-						parentEle 		= Polymer.dom(item).parentNode;
-
-					// create the label and error message if necessary
-					if (errorMsg) {
-						errorMsgEle = new Strand.FormMessage();
-						errorMsgEle.message = errorMsg;
-						errorMsgEle.type = 'error';
-						Polymer.dom(parentEle).appendChild(errorMsgEle);
-					}
-
-					if (label) {
-						var headerTxt = document.createTextNode(label);
-						fieldHeaderEle = new Strand.Header();
-						fieldHeaderEle.size = 'medium';
-						Polymer.dom(fieldHeaderEle).appendChild(headerTxt);
-						Polymer.dom(parentEle).insertBefore(fieldHeaderEle, field);
-					}
-
-					// store the initial form data
-					// TODO: OR populate it(?) given a formData is provided:
-					this._updateData(name, value);
-
-					// store the form items separately for reference later,
-					// since the formData may not map directly to the items
-					this.formItems[name] = {
-						'field'				: field,
-						'validation'		: validation,
-						'errorMsg'			: errorMsg,
-						'errorMsgEle' 		: errorMsgEle,
-						'fieldHeaderEle' 	: fieldHeaderEle
-					};
-
-				}.bind(this));
-
-				this._initialFormData = this.formData;
-				
-			});
+			// this._canInit = true;
+			// this._initForm(this.data);
+		},
+		
+		_dataChanged: function(newVal, oldVal) {
+			// if (this._canInit) this._initForm(newVal); 
+			this._initForm(newVal);
 		},
 
-		
-		// _dataChanged: function(newVal, oldVal) {
+		_initForm: function(data) {
+			for (var key in data) {
+				var name 			= key,
+					label 			= this.data[key].label,
+					errorMsg 		= this.data[key].errorMsg,
+					ele 			= Polymer.dom(this).querySelector('[name='+name+']'),
+					parentEle 		= Polymer.dom(ele).parentNode,
+					// TODO: if there was an initial value in markup - handle it
+					value			= null;
 
-		// 	newVal.forEach(function(item) {
-				
+				if (ele.value || this.data[key].value) {
+					value = ele.value || this.data[key].value;
+				}
 
-		// 	});
+				this._updateData(name, value);
+				this._createLabel(name, parentEle, ele, label);
+				this._createErrorMsg(name, parentEle, errorMsg);
+			}
+		},
 
-		// },
+		_createErrorMsg:function(name, parentEle, msg) {
+			var errorMsgEle = Polymer.dom(parentEle).querySelector('mm-form-message[name='+name+']') || null;
+
+			if (!errorMsgEle) {
+				errorMsgEle = new Strand.FormMessage();
+				errorMsgEle.type = 'error';
+				errorMsgEle.setAttribute('name', name);
+				errorMsgEle.message = msg;
+				Polymer.dom(parentEle).appendChild(errorMsgEle);					
+			} else {
+				errorMsgEle.message = msg;
+			}
+		},
+
+		_createLabel:function(name, parentEle, ele, label) {
+			var labelEle = Polymer.dom(parentEle).querySelector('mm-header[name='+name+']') || null,
+				labelTxt = null;
+
+			if (!labelEle) {
+				labelTxt = document.createTextNode(label);
+				labelEle = new Strand.Header();
+				labelEle.size = 'medium';
+				labelEle.setAttribute('name', name);
+				Polymer.dom(labelEle).appendChild(labelTxt);
+				Polymer.dom(parentEle).insertBefore(labelEle, ele);
+			} else {
+				labelEle.innerHTML = null;
+				labelTxt = document.createTextNode(label);
+				Polymer.dom(labelEle).appendChild(labelTxt);
+			}
+		},
 
 		// *******************************
 		// footer and footer actions:
@@ -236,43 +250,39 @@
 		// *******************************
 		// handle changes within the form
 		_handleChanged: function(e) {
-			var name = e.target.getAttribute('name'),
-				value = e.detail.value,
-				item = this.formItems[name]; // make sure it's an item we actually care about
+			console.log('_handleChanged: ', e, e.detail.value);
+			
+			// var name = e.target.getAttribute('name'),
+			// 	value = e.detail.value,
+			// 	item = this.formItems[name]; // make sure it's an item we actually care about
 
-			if (item && value) {
-				this._updateData(name, item.value);
-				// TODO: re-init the diff
-				this.unsaved = this._diffData();
+			// if (item && value) {
+			// 	this._updateData(name, item.value);
+			// 	this.unsaved = this._diffData();
 
-				// trigger a warning message in the footer
-				if (this.unsaved && this.showWarningMessages) {
-					this.footerMessage = this.footerMessages.warning;
-					this.footerType = 'warning';
-					this._showFooterMessage = true;
-				}
-			}
+			// 	// trigger a warning message in the footer
+			// 	if (this.unsaved && this.showUnsavedMessage) {
+			// 		this.footerMessage = this.footerMessages.warning;
+			// 		this.footerType = 'warning';
+			// 		this._showFooterMessage = true;
+			// 	}
+			// }
 		},
 
 		_updateData: function(name, value) {
-			if (value && typeof(value) === 'object') {
-				for (var subkey in value) {
-					this.formData[subkey] = value[subkey];
-				}
-			} else {
-				this.formData[name] = value;
-			}
+			this._formData[name] = value;
+			console.log(this._formData);
 		},
 
 		_diffData: function() {
-			var diff = [];
+			// var diff = [];
 			
-			for (var key in this.formData) {
-				if (key !== this._initialFormData[key]) {
-					diff.push(key);	
-				}
-			}
-			return diff.length > 0;
+			// for (var key in this.formData) {
+			// 	if (key !== this._initialFormData[key]) {
+			// 		diff.push(key);	
+			// 	}
+			// }
+			// return diff.length > 0;
 		},
 
 		// *******************************
@@ -293,60 +303,58 @@
 		},
 
 		serializeForm: function() {
-			var invalid = [],
-				valid = [];
+			// var invalid = [],
+			// 	valid = [];
 
-			// UI validation pass:
-			// console.log('serializeForm');
-			// TODO: this should be formItems
-			for (var key in this.formItems) {
-				var item 			= this.formItems[key]
-					value 			= item.field.value,
-					validation		= item.validation,
-					isValid 		= false;
+			// // validation pass:
+			// for (var key in this.formItems) {
+			// 	var item 			= this.formItems[key]
+			// 		value 			= item.field.value,
+			// 		validation		= item.validation,
+			// 		isValid 		= false;
 
-				this._updateData(item.field, value);
-				this.unsaved = this._diffData();
+			// 	this._updateData(item.field, value);
+			// 	this.unsaved = this._diffData();
 
-				// validate UI:
-				isValid = this._validateField(validation, value);
+			// 	// validate UI:
+			// 	isValid = this._validateField(validation, value);
 
-				// track invalid & valid fields
-				if (!isValid) {
-					invalid.push(key);
-					item.errorMsgEle.style.display = 'block'; 
-				} else {
-					valid.push(key);
-					item.errorMsgEle.style.display = 'none';
-				}
+			// 	// track invalid & valid fields
+			// 	if (!isValid) {
+			// 		invalid.push(key);
+			// 		item.errorMsgEle.style.display = 'block'; 
+			// 	} else {
+			// 		valid.push(key);
+			// 		item.errorMsgEle.style.display = 'none';
+			// 	}
 
-				// show error state:
-				item.field.error = !isValid;
-			}
+			// 	// show error state:
+			// 	item.field.error = !isValid;
+			// }
 
-			if (invalid.length > 0) {
-				// show messaging in the footer
-				if (this.unsaved) {
-					this.footerType = 'error';
-					this.footerMessage = this.footerMessages.error;
-					this._showFooterMessage = true;
-				}
-			} else {
-				// show messaging in the footer
-				if (this.unsaved) {
-					this.footerType = 'success';
-					this.footerMessage = this.footerMessages.success;
-					this._showFooterMessage = true;
-				}
-			}
+			// if (invalid.length > 0) {
+			// 	// show messaging in the footer
+			// 	if (this.unsaved) {
+			// 		this.footerType = 'error';
+			// 		this.footerMessage = this.footerMessages.error;
+			// 		this._showFooterMessage = true;
+			// 	}
+			// } else {
+			// 	// show messaging in the footer
+			// 	if (this.unsaved) {
+			// 		this.footerType = 'success';
+			// 		this.footerMessage = this.footerMessages.success;
+			// 		this._showFooterMessage = true;
+			// 	}
+			// }
 
-			// fire a serialize-form event:
-			this.fire('serialize-form', {
-				isValid: !invalid.length > 0,
-				invalidFields: invalid,
-				validFields: valid,
-				data: this.formData 
-			});
+			// // fire a serialize-form event:
+			// this.fire('serialize-form', {
+			// 	isValid: !invalid.length > 0,
+			// 	invalidFields: invalid,
+			// 	validFields: valid,
+			// 	data: this.formData 
+			// });
 		},
 		
 		// *******************************
