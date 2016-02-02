5c96bf2b038e6d3922a1d27efe05f294827409ed
Dan Lasky <dlasky@mediamath.com>
2016-02-02 11:36:22 -0600

    model fixes

--- src/mm-model/mm-model.js
+++ src/mm-model/mm-model.js
@@ -10,124 +10,18 @@
 		is:"mm-model",
 
 		properties: {
-			_sync:{
-				type:StrandLib.Sync,
-				value:function() {
-					return new StrandLib.Sync();
-				}
-			},
-			_model:{
-				type:StrandLib.Model,
-				value:function() {
-					return new StrandLib.Model(null, null, this._sync);
-				}
-			},
 			collection:{
 				type:Strand.Collection,
 				value:null,
-			},
-			adapter:{
-				type:String,
-				value:"Sync",
-			},
-			defaults:{
-				type:Object,
-				value: function() {
-					return {};
-				}
 			}
 		},
 
 		behaviors: [
+			StrandLib.Sync.getBehavior(),
+			StrandLib.Model.getBehavior(),
 			StrandTraits.DomSyncable,
 			StrandTraits.Refable
 		],
 
-		ready:function() {
-			this._model.addEventListener("data-changed", function() { 
-				this.fire("data-changed"); 
-			}.bind(this));
-		},
-
-		init: function(data) {
-			this._model.init(data || this.defaults);
-		},
-		
-		adapterChanged: function(oldAdapter, newAdapter) {
-			// if (typeof newAdapter === "string" && window[newAdapter]) {
-			// 	this.adapterInstance = new window[newAdapter]();
-			// 	this.adapterInstance.target = this;
-			// 	this.adapterInstance.auto = this.auto;
-			// }
-		},
-
-		autoChanged: function(oldAuto, newAuto) {
-			if (this.adapterInstance)
-				this.adapterInstance.auto = newAuto;
-		},
-
-		save: function() {
-			return this.connection.post();
-		},
-
-		update: function() {
-			return this.connection.put();
-		},
-
-		fetch: function() {
-			return this.connection.get();
-		},
-		
-		destroy: function() {
-			return this.connection.delete();
-		},
-
-		clear: function() {
-			this._model.clear();
-		},
-
-		getData: function(path) {
-			return this._model.get(path);
-		},
-
-		setData: function(path, value) {
-			this._model.set(path, value);
-		},
-
-		toJSON: function() {
-			return this._model.toJSON();
-		},
-
-		get connection() {
-			return this._model.connection;
-		},
-
-		//proxy accessors to _model.data
-
-		get mId() {
-			return this._model.mId;
-		},
-
-		get cId() {
-			return this._model.cId;
-		},
-
-		set mId(i) {
-			this._model.mId = i;
-		},
-
-		set cId(i) {
-			this._model.cId = i;
-		},
-
-		get data() {
-			return this._model.data;
-		},
-
-		set data(i) {
-			this._model.data = i;
-			this.fire("data-changed");
-		}
-
 	});
-})(window.Strand = window.Strand || {}); 
+})(window.Strand = window.Strand || {});
