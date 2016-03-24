848a94308c70c3d5e6c3091af978b8aa2e91122b
Anthony Koerber <akoerber@mediamath.com>
2016-03-24 17:44:47 -0500

    mm-inline-edit stub

---
+++ src/mm-inline-edit/mm-inline-edit.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright (c) 2015 MediaMath Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at http://mediamath.github.io/strand/LICENSE.txt
+
+*/
+(function(scope) {
+
+	scope.InlineEdit = Polymer({
+		is: 'mm-inline-edit',
+
+		behaviors: [
+			StrandTraits.Resolvable,
+			StrandTraits.Stylable,
+			StrandTraits.Refable
+		],
+
+		properties: {
+			
+		},
+
+		
+
+	});
+
+})(window.Strand = window.Strand || {});
