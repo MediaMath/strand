f08c80b82bcc41e46f36ebc108d06f9ffb1ccff1
Anthony Koerber <akoerber@mediamath.com>
2016-03-30 16:59:38 -0500

    WIP input editor

--- src/mm-inline-edit/mm-inline-edit.js
+++ src/mm-inline-edit/mm-inline-edit.js
@@ -14,56 +14,106 @@
 		behaviors: [
 			StrandTraits.Resolvable,
 			StrandTraits.Stylable,
-			StrandTraits.Refable
+			StrandTraits.Refable,
+			StrandTraits.PositionableEditor,
+			StrandTraits.Keyboardable,
+			StrandTraits.Stackable
 		],
 
 		// type: entity | collection | date
+		TYPE_ENTITY: 'entity',
+		TYPE_COLLECTION: 'collection',
+		TYPE_DATE: 'date',
 
 		properties: {
 
-			value: {
-				type: Object,
-				value: 'cheese'
+			type: {
+				type: String,
+				value: function() {
+					return this.TYPE_ENTITY
+				},
+				notify: true
 			},
-
-			showEditor: {
-				type: Boolean,
-				value: false
-			}
-
+			value: {
+				type: Object
+			},
+			model: Object,
+			field: {
+				type: String,
+				observer: '_fieldChanged'
+			},
 			// entity: {
 			// 	type: Object,
 			// 	value: true
 			// },
-
 			// collection: {
 			// 	type: Object
 			// },
+			_scope: {
+				type: Object,
+				value: function() { return this; }
+			},
+			_panel: {
+				type: Object,
+				value: function() { return this.$.panel; }
+			},
+			_target: {
+				type: Object,
+				value: function() { return this.$.target; }
+			},
+			_stackTarget: {
+				type: Object,
+				value: function() { return this.$.panel; }
+			},
 
 		},
 
-		_editClick: function(e) {
+		listeners: {
+			'keydown': '_handleKeydown'
+		},
+
+		_edit: function(e) {
+			e.preventDefault();
 			this._beginEdit();
 		},
 
-		_beginEdit: function() {
-			// position the resulting editor
-			var targetMetrics = Measure.getBoundingClientRect(this.$.editable);
-			this.showEditor = true;
-
+		_handleKeydown: function(e) {
+			this._routeKeyEvent(e);
 		},
 
-		_isInput: function(value, showEditor) {
+		_beginEdit: function() {
+			// TODO: probably more setup necessary here
+			// for the other types of edit scenarios
+			this.open();
+		},
 
-			if (typeof value === 'string') {
-				return true && showEditor;
-			} else {
-				return false && !showEditor;
+		_fieldChanged: function(newVal) {
+			console.log(newVal);
+		},
+
+		_onEnter: function() {
+			if (this.type === this.TYPE_ENTITY) {
+				// console.log('enter pressed: ', this.value);
+				// TODO: update the model 
+				// console.log(this.model);
+				// this.set(this.model[this.field], this.value);
+				var path = String('model.' + this.field);
+				this.set(path, this.value);
+				this.close();
 			}
+		},
 
-		}
+		_typeInput: function(type) {
+			return type === this.TYPE_ENTITY;
+		},
 
+		_typeCollection: function(type) {
+			return type === this.TYPE_COLLECTION;
+		},
 
+		_typeDate: function(type) {
+			return type === this.TYPE_DATE;
+		},
 
 	});
 
