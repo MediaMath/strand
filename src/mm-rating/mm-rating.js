afa955a74468be3fb23904ccfefc2b3908fd45ab
sawant_d <dsawant002@gmail.com>
2016-04-11 16:58:38 +0530

    Review comments fixed..

--- src/mm-rating/mm-rating.js
+++ src/mm-rating/mm-rating.js
@@ -1,12 +1,10 @@
 ( function (scope) {
     scope.Rating = Polymer({
         is: 'mm-rating',
-        hovered: false,
+        behaviors: [
+            StrandTraits.Stylable
+        ],
         properties: {
-            icon: {
-                type: String,
-                value: 'star'
-            },
             customIcon: {
                 type: String,
                 value: null
@@ -35,37 +33,26 @@
                 type: Boolean,
                 value: false
             },
-            values: {
-                type: Object,
-                value: {0: 'Poor', 1: 'Need Improvement', 2: 'Reasonable', 3: 'Good', 4: 'Excellent'}
+            data: {
+                type: Array,
+                value: function() {
+                    return [{name: 'Poor'}, {name: 'Need Improvement'}, {name: 'Reasonable'}, {name: 'Good'}, {name: 'Excellent'}];
+                }
             }
         },
-
         ready: function() {
             this._defaultNoOfIcons = [];
-            if(typeof this.values === 'string'){
-                var greetObj = eval("("+ this.values +")");
-                this._noOfIcons = Object.keys(greetObj).length;
-            } else if(typeof this.values === 'object'){
-                this._noOfIcons = Object.keys(this.values).length;
-            }
+            this._noOfIcons = this.data.length;
             for (var i = this._noOfIcons - 1; i >= 0; i--) {
                 this._defaultNoOfIcons.push(i+1);
             }
         },
-        isCustomIcon: function() {
-            return this.customIcon;
-        },
-        isActive: function(index) {
-            if ((index - this._noOfIcons) * -1 == this.rating) {
-                return "active";
+        isCustomIcon: function(item) {
+            if(item) {
+                if (this.customIcon === null && item.icon === undefined) return true;
+            } else {
+                return this.customIcon;
             }
-        },
-        isDisplayTotals: function(showtotals) {
-            return showtotals;
-        },
-        isDisplayGreetings: function(showgreetings) {
-            return showgreetings;
         },
         calculateRating: function(e) {
             deep = Polymer.dom(this.root);
@@ -77,37 +64,28 @@
                     deep.querySelector('[data-index="'+ indexOld +'"]').classList.remove("active");
                 }
                 deep.querySelector('[data-index="'+ index +'"]').classList.add("active");
-                if(typeof this.values === 'string'){
-                    var greetObj = eval("("+ this.values +")");
-                    this.greetArr = Object.keys(greetObj).map(function (key) {return greetObj[key]});
-                    this.finalGreet = this.greetArr.reverse();
-                    this.greeting = this.finalGreet[index];
-                }
+                var greetObj = this.data;
+                this.greetArr = Object.keys(greetObj).map(function (key) {return greetObj[key]});
+                this.finalGreet = this.greetArr.reverse();
+                this.greeting = this.finalGreet[index];
             }
         },
         _overHandler: function(e) {
             var index = e.model.index;
-            if(typeof this.values === 'string'){
-                var greetObj = eval("("+ this.values +")");
-                this.greetArr = Object.keys(greetObj).map(function (key) {return greetObj[key]});
-                this.finalGreet = this.greetArr.reverse();
-                this.greeting = this.finalGreet[index];
-            } else if(typeof this.values === 'object'){
-                var greetObj = this.values;
-                this.greetArr = Object.keys(greetObj).map(function (key) {return greetObj[key]});
-                this.finalGreet = this.greetArr.reverse();
-                this.greeting = this.finalGreet[index];
-            }
-
+            var greetObj = this.data;
+            this.greetArr = Object.keys(greetObj).map(function (key) {return greetObj[key]});
+            this.finalGreet = this.greetArr.reverse();
+            this.greeting = this.finalGreet[index];
         },
         _outHandler: function(e) {
-            if(typeof this.values === 'string') {
-                var greetObj = eval("(" + this.values + ")");
-                this.greeting = greetObj[this.rating - 1];
-            } else if(typeof this.values === 'object') {
-                var greetObj = this.values;
-                this.greeting = greetObj[this.rating - 1];
+            this.greeting = this.data[this.rating - 1];
+        },
+        updateClass: function(index) {
+            var o = {};
+            if ((index - this._noOfIcons) * -1 == this.rating) {
+                o.active = true;
             }
+            return this.classBlock(o);
         }
     });
 })(window.Strand = window.Strand || {});
