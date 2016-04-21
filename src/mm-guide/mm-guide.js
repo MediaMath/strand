fc3a7b9e1c483dbbec632799d1be2530b10922d8
Anthony Koerber <akoerber@mediamath.com>
2016-04-21 17:00:48 -0500

    mm-guide stub

---
+++ src/mm-guide/mm-guide.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright (c) 2015 MediaMath Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at http://mediamath.github.io/strand/LICENSE.txt
+
+*/
+(function(scope) {
+
+	scope.Guide = Polymer({
+		is: 'mm-guide',
+
+		behaviors: [
+			StrandTraits.Resolvable,
+			StrandTraits.Refable
+		],
+
+		properties: {
+			
+		},
+
+		attached: function() {
+			
+		},
+
+		detached: function() {
+			
+		},
+
+	});
+
+})(window.Strand = window.Strand || {});
