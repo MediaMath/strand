b59f09da9540c9a1a6e63d8248716b468bab2bc1
Anthony Koerber <akoerber@mediamath.com>
2016-04-22 16:49:09 -0500

    add strand-guide-tooltip boilerplate

---
+++ src/strand-guide-tooltip/strand-guide-tooltip.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright (c) 2015 MediaMath Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at http://mediamath.github.io/strand/LICENSE.txt
+
+*/
+(function(scope) {
+
+	scope.GuideTooltip = Polymer({
+		is: 'strand-guide-tooltip',
+
+		behaviors: [
+			StrandTraits.Resolvable,
+			StrandTraits.Refable
+		],
+
+		properties: {
+			
+		},
+
+		attached: function() {
+			
+		},
+
+		detached: function() {
+			
+		},
+
+	});
+
+})(window.Strand = window.Strand || {});
