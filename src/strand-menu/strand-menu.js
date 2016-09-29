3cfe9a0f64d5c5ee513b70fdfc0c85648d79b335
Dan Lasky <dlasky@mediamath.com>
2016-09-29 15:40:57 -0500

    add data support and selected event to menu

--- src/strand-menu/strand-menu.js
+++ src/strand-menu/strand-menu.js
@@ -9,13 +9,28 @@
 			StrandTraits.AutoTogglable,
 			StrandTraits.PositionablePanel,
 			StrandTraits.Stackable,
+			StrandTraits.LightDomGettable,
 			StrandTraits.Refable
 		],
+
+		listeners: {
+			'click':'_handleSelect'
+		},
 
 		attached: function() {
 			this.async(function() {
 				if (this._target) this._target.style.cursor = 'pointer';
 			});
+		},
+
+		_handleSelect: function(e) {
+			if (e.target !== this.$.container) {
+				this.fire('selected',{
+					item: e.target,
+					index: this.getLightDOM().indexOf(e.target),
+					value: e.target.getAttribute('value') || e.target.textContent.trim()
+				});
+			}
 		}
 	});
 
