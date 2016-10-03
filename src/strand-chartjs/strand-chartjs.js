54c07e02abfb20417aabe403e1be5ef61bfe0780
Anthony Koerber <akoerber@mediamath.com>
2016-10-03 18:04:25 -0500

    Basic working chartjs

--- src/strand-chartjs/strand-chartjs.js
+++ src/strand-chartjs/strand-chartjs.js
@@ -15,18 +15,140 @@
 			StrandTraits.Refable
 		],
 
+		TYPE_LINE: 'line',
+		TYPE_BAR: 'bar',
+		TYPE_HORIZONTAL_BAR: 'horizontalBar',
+		TYPE_PIE: 'pie',
+		TYPE_DOUGHNUT: 'doughnut',
+		TYPE_POLAR_AREA: 'polarArea',
+		TYPE_RADAR: 'radar',
+		TYPE_BUBBLE: 'bubble',
+
 		properties: {
+			// strand specific
+			// type: {
+			// 	type: String,
+			// 	value: function() {
+			// 		return this.TYPE_LINE;
+			// 	}
+			// },
+
+			globalFont: {
+				type: Object,
+				value: {
+					defaultFontColor: '#666',
+					defaultFontFamily: '"Arimo", sans-serif',
+					defaultFontSize: 12,
+					defaultFontStyle: 'normal',
+					responsive: false
+				}
+			},
+
 			width: {
 				type: Number,
 				value: 500
 			},
+
 			height: {
 				type: Number,
 				value: 500
+			},
+
+			context: {
+				type: Object,
+				value: function() {
+					return this.$.context
+				}
+			},
+
+			chart: {
+				type: Object
+			},
+
+			config: {
+				type: Object,
+				observer: '_configChanged'
 			}
+
+			// _configuration: {
+			// 	type: Object,
+			// 	value: {
+			// 		type: '',
+			// 		labels: [],
+			// 		datasets: [],
+			// 		options: {}
+			// 	}
+			// },
+
+
+			// chartjs 
+			// datasets: {
+			// 	type: Array,
+			// 	value: [],
+			// 	notify: true
+			// },
+			// labels: {
+			// 	type: Array,
+			// 	value: [],
+			// 	notify: true
+			// },
+			// xLabels: {
+			// 	type: Array,
+			// 	value: [],
+			// 	notify: true
+			// },
+			// yLabels: {
+			// 	type: Array,
+			// 	value: [],
+			// 	notify: true
+			// },
+			// options: {
+			// 	type: Object,
+			// 	value: {},
+			// 	notify: true
+			// },
+
+
+
 		},
 
-		// TODO
+		// observers: [
+		// 	'_updateChart(type, labels, datasets, options)'
+		// ],
+
+		// TODO - actually useful stuff:
+		// ready: function() {
+		// 	if (this.config) {
+		// 		this._updateChart();
+		// 	}
+		// },
+
+		_configChanged: function(newVal, oldVal) {
+			if (newVal) this._updateChart();
+		},
+
+
+		_updateChart: function() {
+			// this._configuration.type = this.type;
+			// this._configuration.data = {};
+			// this._configuration.data.datasets = this.datasets;
+			// this._configuration.data.labels = this.labels;
+			// this._configuration.options = this.options;
+
+			// if (this.chart) {
+			// 	this.chart.update();
+			// } else {
+			// 	this.chart = new Chart(this.context, this._configuration);
+			// }
+
+			if (!this.chart) {
+				this.chart = new Chart(this.context, this.config);
+			} else {
+				this.chart.update();
+			}
+
+		},
+
 
 		_updateStyle: function(width, height, fitparent) {
 			return this.styleBlock({
